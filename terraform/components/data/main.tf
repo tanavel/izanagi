be77ab6b9a9b64dd7ede42a62729626e087dfb80
tanavel <tanavel1118@gmail.com>
2021-06-07 11:08:41 +0900

    databaseの構築

---
+++ terraform/components/data/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+#================================================#
+# RDS
+#================================================#
+resource "aws_db_instance" "this" {
+  # required
+  instance_class              = "db.t2.micro"
+  allocated_storage           = 20
+  engine                      = "mysql"
+  username                    = "root"
+  password                    = "password"
+  # optional
+  engine_version              = "8.0.20"
+  skip_final_snapshot         = true
+  apply_immediately           = true
+  tags = {
+    Name = "tanavel-prd-db-1"
+    Env  = "prd"
+    Sys  = "tanavel"
+  }
+}
