e062012c7220cb76ac35a4f4a96da77e2eb33d1f
tanavel <tanavel1118@gmail.com>
2021-07-22 01:52:50 +0900

    アプリケーション層を構築する

--- terraform/components/network/outputs.tf
+++ terraform/components/network/outputs.tf
@@ -1,3 +1,27 @@
 output "network_vpc_id" {
   value = aws_vpc.this.id
 }
+
+output "network_subnet_public_1_id" {
+  value = aws_subnet.public_1.id
+}
+
+output "network_subnet_public_2_id" {
+  value = aws_subnet.public_2.id
+}
+
+output "network_subnet_public_3_id" {
+  value = aws_subnet.public_3.id
+}
+
+output "network_subnet_private_1_id" {
+  value = aws_subnet.private_1.id
+}
+
+output "network_subnet_private_2_id" {
+  value = aws_subnet.private_2.id
+}
+
+output "network_subnet_private_3_id" {
+  value = aws_subnet.private_3.id
+}
